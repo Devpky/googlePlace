63f8805fa3dc3369aa1c05c3525510ccd3ca25d7
pavan yadav <pavan.yadav@invetechsolutions.com>
2018-07-17 15:41:37 +0530

    ok
    first Init

--- googlePlace/ViewController.swift
+++ googlePlace/ViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+
+class ViewController: UIViewController,GMSMapViewDelegate
+{
+    
+    
+    @IBOutlet var MytextField: UITextField!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
+    
+    
+    
+    
+    @IBAction func openStartLocation(_ sender: UIButton)
+    {
+        
+        let autoCompleteController = GMSAutocompleteViewController()
+        autoCompleteController.delegate = self
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        UISearchBar.appearance().setTextColor(color: UIColor.black)
+        
+        self.present(autoCompleteController, animated: true, completion: nil)
     }
+    
+    
+}
 
 
+
+
+// MARK: - GMS Auto Complete Delegate, for autocomplete search location
+extension ViewController: GMSAutocompleteViewControllerDelegate
+{
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("Error \(error)")
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        
+        
+        MytextField.text = place.name
+        //"\(place.coordinate.latitude), \(place.coordinate.longitude)"
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+}
+
+public extension UISearchBar
+{
+    
+    public func setTextColor(color: UIColor)
+    {
+        let svs = subviews.flatMap { $0.subviews }
+        guard let tf = (svs.filter { $0 is UITextField }).first as? UITextField else { return }
+        tf.textColor = color
+    }
+    
 }
 
